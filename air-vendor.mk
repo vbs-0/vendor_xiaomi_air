5c8eff91db4b01a71975d7f781f0209530b23789
vbs <Chvbswami@gmail.com>
2025-12-13 13:24:22 +0530

    Refine proprietary files copying in air-vendor.mk
    Updated the proprietary files copy process to exclude init scripts that conflict with Soong modules.

--- air-vendor.mk
+++ air-vendor.mk
@@ -8,11 +8,15 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),air)
 
-# Copy all proprietary files
+# Copy proprietary files, excluding init scripts that conflict with Soong modules
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor,$(TARGET_COPY_OUT_VENDOR))
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/system,$(TARGET_COPY_OUT_SYSTEM))
 
 endif
+
